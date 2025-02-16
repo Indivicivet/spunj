8473c79138f41d20f635fa0db0a195e8ae664de3
indi <indivicivet@gmail.com>
2025-02-16 22:10:44 +0000

    onready all sprite loading

--- GodotProject/scripts/penny_3d.gd
+++ GodotProject/scripts/penny_3d.gd
@@ -4,16 +4,16 @@ var rng = RandomNumberGenerator.new()
 
 var is_wet: bool = false
 
+@onready var sprite_container_face : Node3D = get_node("FaceSpriteContainer")
+@onready var sprite_face_new : Sprite3D = sprite_container_face.get_node("FaceNew")
+@onready var sprite_face_old : Sprite3D = sprite_container_face.get_node("FaceOld")
+@onready var sprite_container_rim : Node3D = get_node("RimSpriteContainer")
+@onready var sprite_rim_new : Sprite3D = sprite_container_rim.get_node("RimNew")
+@onready var sprite_rim_old : Sprite3D = sprite_container_rim.get_node("RimOld")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	rng.randomize()
-	
-	var sprite_container_face : Node3D = get_node("FaceSpriteContainer")
-	var sprite_face_new : Sprite3D = sprite_container_face.get_node("FaceNew")
-	var sprite_face_old : Sprite3D = sprite_container_face.get_node("FaceOld")
-	var sprite_container_rim : Node3D = get_node("RimSpriteContainer")
-	var sprite_rim_new : Sprite3D = sprite_container_rim.get_node("RimNew")
-	var sprite_rim_old : Sprite3D = sprite_container_rim.get_node("RimOld")
 	
 	if rng.randf() < 0.5:
 		sprite_face_new.texture = load("res://assets/sprites/coins/1p/1p_reverse_new.png")
