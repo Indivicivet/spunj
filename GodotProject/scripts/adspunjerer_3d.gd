741e83b23ce31afb7ffa0f80307627521357fdc4
indi <indivicivet@gmail.com>
2025-03-28 23:12:40 +0000

    explicit "push penny" logic - different janky physics

--- GodotProject/scripts/adspunjerer_3d.gd
+++ GodotProject/scripts/adspunjerer_3d.gd
@@ -40,3 +40,9 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+	for i in range(get_slide_collision_count()):
+		var collider = get_slide_collision(i).get_collider()
+		if collider is Penny3D:
+			var dp = collider.position - position;
+			dp.z = 0;
+			collider.apply_central_impulse(dp * 1000);
